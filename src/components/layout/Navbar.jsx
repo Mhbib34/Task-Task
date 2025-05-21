a89b5a26b23c2e59949a19ed4ad1d769664eadf1
Mhbib34 <mhabib34official@gmail.com>
2025-05-21 15:23:35 +0700

    Adding animation at Navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import AvatarImage from "../fragment/AvatarImage";
+/* eslint-disable */
+import { motion, AnimatePresence } from "framer-motion";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,16 +13,33 @@ const Navbar = () => {
         <i className="fa-solid fa-bars text-2xl text-white "></i>
       </div>
       <AvatarImage image={avatar} className="w-10 h-10" />
-      <div
-        className={`${
-          isOpen ? "block" : "hidden"
-        } h-screen bg-black absolute left-0 top-0 sm:w-[400px] w-[250px] z-50`}
-      >
-        <i
-          onClick={() => setIsOpen(false)}
-          className="fa-solid fa-xmark text-2xl text-white cursor-pointer absolute top-4 right-4"
-        ></i>
-      </div>
+      <AnimatePresence>
+        {isOpen && (
+          <>
+            <motion.div
+              className="fixed inset-0  bg-opacity-50 z-40"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              onClick={() => setIsOpen(false)}
+            />
+
+            <motion.div
+              className="fixed top-0 left-0 h-screen bg-black sm:w-[400px] w-[250px] z-50 p-4"
+              initial={{ x: "-100%" }}
+              animate={{ x: 0 }}
+              exit={{ x: "-100%" }}
+              transition={{ type: "tween", duration: 0.3 }}
+            >
+              <i
+                onClick={() => setIsOpen(false)}
+                className="fa-solid fa-xmark text-2xl text-white cursor-pointer absolute top-4 right-4"
+              ></i>
+              {/* Konten sidebar di sini */}
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
