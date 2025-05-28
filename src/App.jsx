93b74e9b5b0ef6099bcf5a3666d9744838a1f80f
Mhbib34 <mhabib34official@gmail.com>
2025-05-28 09:44:07 +0700

    Adding Progress Task Page

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import TodoAdd from "./pages/TodoAdd";
 import "react-datepicker/dist/react-datepicker.css";
 import TodoDetail from "./pages/TodoDetail";
 import Done from "./pages/Done";
+import Progress from "./pages/Progress";
 
 function App() {
   const [showSplash, setShowSplash] = useState(false);
@@ -57,6 +58,7 @@ function App() {
         <Route path="/todo/add" element={<TodoAdd />} />
         <Route path="/todo/:id" element={<TodoDetail />} />
         <Route path="/done" element={<Done />} />
+        <Route path="/progress" element={<Progress />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Progress.jsx
@@ -0,0 +1,74 @@
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/layout/Navbar";
+import React, { useState } from "react";
+import { colorMap } from "../utils/colorCard";
+import BarTodo from "../components/fragment/BarTodo";
+import TaskFilter from "../components/fragment/TaskFilter";
+import CardScroollTimeline from "../components/layout/CardScroolTimeline";
+import CardScroollDekstop from "../components/layout/CardScrollDekstop";
+import { filterTasks } from "../utils/taskUtils";
+
+const Progress = () => {
+  const [filter, setFilter] = useState("today");
+  const [filterDate, setFilterDate] = useState("");
+  const allTasks = JSON.parse(localStorage.getItem("todos")) || [];
+  const task = filterTasks(allTasks, filter, filterDate);
+  const taskProgress = task.filter((t) => t.status === "Progress");
+  console.log(task);
+  console.log(taskProgress);
+
+  const navigate = useNavigate();
+  const colorKeys = Object.keys(colorMap);
+
+  const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colorKeys.length);
+    return colorKeys[randomIndex];
+  };
+
+  const handleClick = (item) => {
+    console.log(item);
+    navigate(`/todo/${item.id}`);
+  };
+
+  return (
+    <div className="bg-black">
+      <Navbar />
+      <div className="md:px-10 px-5 pb-5 pt-2 bg-black min-h-screen">
+        <div className="mt-10 flex flex-col gap-10 sm:flex-row sm:items-center sm:justify-between pr-5 ">
+          <BarTodo
+            icon="plus"
+            text="Add Task"
+            onClick={() => navigate("/todo/add")}
+          />
+          <TaskFilter
+            filter={filter}
+            setFilter={setFilter}
+            filterDate={filterDate}
+            setFilterDate={setFilterDate}
+          />
+        </div>
+
+        <div className="sm:hidden">
+          <CardScroollTimeline
+            onClick={handleClick}
+            task={taskProgress}
+            filter={filter}
+            filterDate={filterDate}
+          />
+        </div>
+
+        <div className="mt-10">
+          <CardScroollDekstop
+            task={taskProgress}
+            getRandomColor={getRandomColor}
+            filter={filter}
+            filterDate={filterDate}
+            onClick={handleClick}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Progress;
